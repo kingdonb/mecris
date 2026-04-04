03038bbef3ac2fe42a1601f05102b2949fb59b4c
Kingdon B <kingdon@urmanac.com>
2026-04-04 08:42:50 -0400

    chore: remove temporary diagnostic scripts

--- check_env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-vars_to_check = ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_WHATSAPP_FROM', 'TWILIO_TO_NUMBER', 'TWILIO_WHATSAPP_TEMPLATE_SID', 'NEON_DB_URL']
-for v in vars_to_check:
-    val = os.getenv(v)
-    print(f"{v}: {'Set' if val else 'MISSING'}")
