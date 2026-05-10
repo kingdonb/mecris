a2f49da8f40e8e9a672153aa813bc9410bb9172a
Kingdon B <kingdon@urmanac.com>
2026-05-09 22:50:02 -0400

    fix(moon-phase): synchronize wasm filename in MainActivity

---
+++ tools/moon-phase-clock/brain/target/wasm32-wasip1/release/deps/moon_phase_brain.d
@@ -0,0 +1,5 @@
+/Users/yebyen/w/mecris/tools/moon-phase-clock/brain/target/wasm32-wasip1/release/deps/moon_phase_brain.d: src/lib.rs
+
+/Users/yebyen/w/mecris/tools/moon-phase-clock/brain/target/wasm32-wasip1/release/deps/moon_phase_brain.wasm: src/lib.rs
+
+src/lib.rs:
---
+++ tools/moon-phase-clock/brain/target/wasm32-wasip1/release/moon-phase.d
@@ -0,0 +1,1 @@
+/Users/yebyen/w/mecris/tools/moon-phase-clock/brain/target/wasm32-wasip1/release/moon-phase.wasm: /Users/yebyen/w/mecris/tools/moon-phase-clock/brain/src/main.rs
