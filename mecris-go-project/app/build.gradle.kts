c95b7a4ab3cb2897e64218c7bc5e9dd261ecbb36
Kingdon B <kingdon@urmanac.com>
2026-07-19 19:12:20 -0400

    chore: bump version to 0.0.1-rc.2

--- mecris-go-project/app/build.gradle.kts
+++ mecris-go-project/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 26
-        versionName = "0.0.1-rc.1"
+        versionName = "0.0.1-rc.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
